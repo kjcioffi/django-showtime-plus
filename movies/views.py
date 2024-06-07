5db77e9df321e3902f574a50352ffa7dbe6be247
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-06-07 08:58:43 -0400

    Development completed for initial test.

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from movies.movie_api_utils import MovieApiUtils
 
-# Create your views here.
+
+def index(request):
+    movie_utils = MovieApiUtils()
+    movies = movie_utils.get_movies_now_playing()
+    return render(request, "movies/index.html", {"movies": movies})
