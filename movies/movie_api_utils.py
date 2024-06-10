3962b6d63e90f197dc58bd28af44b73e7a63a5b5
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-06-10 15:59:28 -0400

    Refactored MovieApiUtils to filter movies by a beginning date.

--- movies/movie_api_utils.py
+++ movies/movie_api_utils.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+import datetime
 import environ
 import requests
 
@@ -5,7 +7,15 @@ import requests
 class MovieApiUtils:
     env = environ.Env()
 
-    MOVIES_IN_THEATERS = "https://api.themoviedb.org/3/movie/now_playing"
+    MOVIES_IN_THEATERS = (
+        "https://api.themoviedb.org/3/discover/movie?" +
+        "include_adult=false&" +
+        "include_video=false&" +
+        "language=en-US&" +
+        "page=1&" +
+        "primary_release_date.gte={date}&" +
+        "sort_by=popularity.desc"
+    )
 
     API_KEY = env("TMDB_API_KEY", default="")
 
@@ -15,4 +25,9 @@ class MovieApiUtils:
     }
 
     def get_movies_now_playing(self):
-        return requests.get(self.MOVIES_IN_THEATERS, headers=self.headers)
+        today = timezone.now().date()
+        month_and_half_ago = today - datetime.timedelta(days=45)
+        return requests.get(
+            self.MOVIES_IN_THEATERS.format(date=month_and_half_ago.isoformat()),
+            headers=self.headers,
+        )
