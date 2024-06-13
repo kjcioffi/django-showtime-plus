65ae7e798ca7a668ee1942bb5838521546054280
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-06-13 09:51:44 -0400

    Refactored MovieApiUtils test suite for improved test coverage.

--- movies/tests/test_movie_api_utils.py
+++ movies/tests/test_movie_api_utils.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from requests import Response
 
 from movies.exceptions import MovieApiException
 from movies.movie_api_utils import MovieApiUtils
@@ -16,20 +15,18 @@ class TestMovieApiUtils(TestCase):
             "Invalid API key: You must be granted a valid key.",
             "Ensure your TMDB API key is valid and functioning properly.",
         )
-        self.assertEqual(response["success"], True)
 
-    def test_movies_now_playing_fail(self):
-        movie_api = MovieApiUtils()
-
+    def test_invalid_api_key(self):
         with self.assertRaises(MovieApiException):
+            movie_api = MovieApiUtils()
             movie_api.api_key = ""
             movie_api.headers["Authorization"] = "Bearer ''"
             movie_api.get_movies_now_playing()
 
     def test_movies_now_playing_success(self):
         movies = self.movie_api_utils.get_movies_now_playing()
-        self.assertIsInstance(movies, Response)
-        self.assertTrue(movies.json()["results"])
+        self.assertIsInstance(movies, dict)
+        self.assertTrue(movies["results"])
 
-        for movie in movies.json()["results"]:
+        for movie in movies["results"]:
             self.assertTrue(movie)
